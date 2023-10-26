a13311566a53e2ce34c6ae87b4846340128268e6
pragyan69 <prajyanqw@gmail.com>
2023-10-26 11:22:19 +0530

    changes done in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,18 +16,18 @@ function App() {
   
   const [walletAddress, setWalletAddress] = useState('');
    const tokenMetadata = useRef(null);
-  // const tokenTransfer = useRef(null);
-  // const tokenHolders = useRef(null);
+  
+  const customWallet = useRef(null)
 
   return (
     
        <div className="bg-blue-100 min-h-screen m-0 p-0"> {/* Ensure no padding at the top */}
     <div className="mt-5 p-0"> {/* Ensure no margin at the top */}
     <WalletContext.Provider value={{ walletAddress, setWalletAddress }}>
-      <NavbarComponent refs = {{tokenMetadata}}/>
+      <NavbarComponent refs = {{tokenMetadata, customWallet}}/>
       
       <TransactionChart />
-      <SpecificWalletTransactionChart />
+      <SpecificWalletTransactionChart ref = {customWallet} />
      
     </WalletContext.Provider>
     </div>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -29,7 +29,7 @@ const NavbarComponent = ({ refs }) => {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DAO Stats</span>
                 </a>
                 <div className="flex md:order-2">
                     {/* Wallet address input and button */}
@@ -55,13 +55,13 @@ const NavbarComponent = ({ refs }) => {
                 <div className={`items-center justify-between ${isOpen ? '' : 'hidden'} md:flex md:w-auto md:order-1`} id="navbar-search">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <a onClick={() => scrollToComponent(refs.tokenMetadata)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Token Metadata</a>
+                            <a onClick={() => scrollToComponent(refs.tokenMetadata)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 hover:">Token Metadata</a>
                         </li>
                         {/* <li>
                             <a onClick={() => scrollToComponent(refs.tokenTransfers)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Token Transfers</a>
                         </li> */}
                         <li>
-                            <a onClick={() => scrollToComponent(refs.tokenHolders)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Custom WalletData</a>
+                            <a onClick={() => scrollToComponent(refs.customWallet)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Custom WalletData</a>
                         </li>
                     </ul>
                 </div>
